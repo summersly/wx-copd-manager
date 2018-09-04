76be66a108fb1957cd08b478761bac63d4a739f9
LY Sheng <summersly0805@gmail.com>
2018-09-04 10:22:32 +0800

    collecton function fix

--- pages/mine/mine-collection/collection.js
+++ pages/mine/mine-collection/collection.js
@@ -8,41 +8,47 @@ Page({
    */
   data: {
     knoArr: [],
-    knoPage: 2,
+    // knoPage: 2,
   },
 
-  getMoreKno: function (e) {
-    let patientId = wx.getStorageSync('patientid_token')
-    let page = this.data.knoPage
-    request({
-      url: knoUrl + 'type=1&patientId=' + patientId + '&page_index=' + page + '&items_per_page=10'
-    }).then(res => {
-      if (res.data.result.current_item_count <= 0) {
-        wx.showToast({
-          title: '到底啦~',
-          icon: 'none'
-        })
-      } else {
-        let arr = res.data.result.items.map(data => {
-          return {
-            knoId: data.knoId,
-            knoIfFavorite: data.knoIfFavorite,
-            knoName: data.knoName,
-            knoTime: data.knoTime.split(" ")[0],
-            knoIfRead: data.knoIfRead,
-          }
-        })
-        this.setData({
-          knoArr: this.data.knoArr.concat(arr),
-          knoPage: page + 1
-        })
-      }
-    })
-  },
+  // getMoreKno: function (e) {
+  //   let patientId = wx.getStorageSync('patientid_token')
+  //   let page = this.data.knoPage
+  //   request({
+  //     url: knoUrl + 'type=1&patientId=' + patientId + '&page_index=' + page + '&items_per_page=10'
+  //   }).then(res => {
+  //     if (res.data.result.current_item_count <= 0) {
+  //       wx.showToast({
+  //         title: '到底啦~',
+  //         icon: 'none'
+  //       })
+  //     } else {
+  //       let arr = res.data.result.items.map(data => {
+  //         return {
+  //           knoId: data.knoId,
+  //           knoIfFavorite: data.knoIfFavorite,
+  //           knoName: data.knoName,
+  //           knoTime: data.knoTime.split(" ")[0],
+  //           knoIfRead: data.knoIfRead,
+  //         }
+  //       })
+  //       this.setData({
+  //         knoArr: this.data.knoArr.concat(arr),
+  //         knoPage: page - 1
+  //       })
+  //     }
+  //   })
+  // },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.showLoading({
+      title:'加载收藏中~'
+    })
+    setTimeout(()=>{
+      wx.hideLoading()
+    },3000)
     let patientId = wx.getStorageSync('patientid_token')
     request({
       url: knoUrl + 'type=1&patientId=' + patientId + '&page_index=1&items_per_page=10'
@@ -53,17 +59,28 @@ Page({
           icon: 'none'
         })
       } else {
-        let arr = res.data.result.items.map(data => {
-          return {
-            knoId: data.knoId,
-            knoIfFavorite: data.knoIfFavorite,
-            knoName: data.knoName,
-            knoTime: data.knoTime.split(" ")[0],
-            knoIfRead: data.knoIfRead,
+        request({
+          url: knoUrl + 'type=1&patientId=' + patientId + '&page_index=1&items_per_page=' + res.data.result.total_items
+        }).then(res2 => {
+          if (res2.data.result.current_item_count <= 0) {
+            wx.showToast({
+              title: '暂无健康知识收藏',
+              icon: 'none'
+            })
+          } else {
+            let arr = res.data.result.items.map(data => {
+              return {
+                knoId: data.knoId,
+                knoIfFavorite: data.knoIfFavorite,
+                knoName: data.knoName,
+                knoTime: data.knoTime.split(" ")[0],
+                knoIfRead: data.knoIfRead,
+              }
+            })
+            this.setData({
+              knoArr: arr
+            })
           }
-        })
-        this.setData({
-          knoArr: arr
         })
       }
     })
